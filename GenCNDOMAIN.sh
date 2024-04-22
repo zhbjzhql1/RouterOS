c83e0375d8a6b215fdb08a4293090fc795066f3f
lnc1995 <lnc1995@qq.com>
2024-04-23 04:23:52 +0800

    Create GenCNDOMAIN.sh

---
+++ GenCNDOMAIN.sh
@@ -0,0 +1,4 @@
+curl -s https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf | sed -e 's|^server=/\(.*\)/114.114.114.114$|\1|' | grep -Ev -e '^#' -e '^$' > china.txt
+curl -s https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf | sed -e 's|^server=/\(.*\)/114.114.114.114$|\1|' | grep -Ev -e '^#' -e '^$' > google.txt
+curl -s https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf | sed -e 's|^server=/\(.*\)/114.114.114.114$|\1|' | grep -Ev -e '^#' -e '^$' > apple.txt
+sort -u china.txt apple.txt google.txt | sed -e 's/^/add forward-to=$dnsserver type=FWD address-list=CN_List match-subdomain=yes name=/g' -e '1i:global dnsserver 223.5.5.5\n/ip dns static\nremove [/ip dns static find address-list=CN_List]' -e '$a/ip dns cache flush' >CNDOMAIN.RSC
